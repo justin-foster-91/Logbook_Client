75b1e9eebacc5442ca83e9c6eb9f06c377ff6df3
Justin Foster <jfoster91@gmail.com>
2020-09-17 21:19:16 -0400

    Changing favicon link

--- src/Routes/LandingPage/LandingPage.test.js
+++ src/Routes/LandingPage/LandingPage.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import LandingPage from './LandingPage';
 
 it('renders without crashing', () => {
@@ -8,3 +9,10 @@ it('renders without crashing', () => {
   ReactDOM.render(<BrowserRouter><LandingPage /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 })
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create()
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+});
